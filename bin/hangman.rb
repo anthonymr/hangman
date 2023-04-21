896219c13dcc0ae26f1695aba0f31d818338bb7b
Anthony Martin <anthonymartin20@gmail.com>
2023-04-21 15:04:38 -0400

    Create entry point file

---
+++ bin/hangman.rb
@@ -0,0 +1,12 @@
+require_relative 'game'
+require_relative 'arguments'
+
+def main
+  game = Game.new(Arguments.parse[:difficulty])
+  game.play
+
+rescue Interrupt
+  system('cls') || system('clear')
+end
+
+main
